bf23cbe4263d34a95109c40eb77bb85d6ed6410b
js829 <js829@njit.edu>
2017-02-26 23:32:28 -0500

    fix into statement

--- add_category.php
+++ add_category.php
@@ -10,7 +10,7 @@ if ($name == null) {
     require_once('database.php');
 
     // Add the product to the database  
-    $query = 'INSERT INTO categories (categoryName)
+    $query = 'INSERT INTO categories_guitar1 (categoryName)
     	      VALUES (:category_name)';
     $statement = $db->prepare($query);
     $statement->bindValue(':category_name', $name);
